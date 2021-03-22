58c86c11e8d8a99fc6ba8bcab7c0a5384ad90de5
nouha jbeli <jbelinouha9@gmail.com>
2021-03-22 11:35:26 +0100

    fix categories

--- front-end/src/app/components/recettes/recettes.component.ts
+++ front-end/src/app/components/recettes/recettes.component.ts
@@ -10,12 +10,13 @@ import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
 export class RecettesComponent implements OnInit {
   myArray: any = [];
   file: any;
-
+  categorie:any
 
   constructor(private myService: RecetteService,private sanitizer: DomSanitizer,) { }
 
   ngOnInit(): void {
    this.getRecette()
+
   }
   getfile(f: any) {
     this.file = '';
@@ -36,10 +37,29 @@ export class RecettesComponent implements OnInit {
       });
   }
   getRecette(){
-    this.myService.getService().subscribe((data) => {
+    console.log('categorie',this.categorie)
+    this.myService.getService().subscribe((data:any) => {
       console.log(data);
+     if(this.categorie==='Entrée'){
+      this.myArray =data.filter((recette:any) => recette.categorie === 'Entrée')
+     }else if(this.categorie ==='Plat Principal'){
+      this.myArray =data.filter((recette :any)=> recette.categorie === 'Plat Principal')
+
+     }else if(this.categorie === 'Patisseries Recettes'){
+      this.myArray =data.filter((recette:any )=> recette.categorie === 'Patisseries Recettes')
+
+     }else {
       this.myArray = data;
+
+     }
     });
+  }
+
+  handleClick(event: Event) {
+    console.log('Click!', event.target)
+    this.categorie=(event.currentTarget  as HTMLButtonElement).value
+    console.log(this.categorie)
+    this.getRecette()
   }
 
 }
